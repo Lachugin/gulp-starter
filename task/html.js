de5dfb8b156830ee227b98ba0ca8a601fc6111ea
Vladimir Lachugin <lachuginvv@icloud.com>
2022-05-29 21:42:58 +0300

    gulp-webp-task

--- task/html.js
+++ task/html.js
@@ -11,6 +11,7 @@ const notify = require('gulp-notify');
 const fileinclude = require('gulp-file-include');
 const htmlmin = require('gulp-htmlmin');
 const size = require('gulp-size');
+const webpHtml = require('gulp-webp-html');
 
 //  обработка HTML
 const html = () => {
@@ -22,6 +23,7 @@ const html = () => {
       }))
     }))
     .pipe(fileinclude())
+    .pipe(webpHtml())
     .pipe(size({title: 'До сжатия'}))
     .pipe(htmlmin(app.htmlmin))
     .pipe(size({title: 'После сжатия'}))
