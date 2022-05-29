a2258c896be492462db076482618144cd9d210c9
Vladimir Lachugin <lachuginvv@icloud.com>
2022-05-29 11:08:03 +0300

    task completed html

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
-const { src, dest } = require("gulp");
+const { src, dest } = require('gulp');
 
 // плагины
 const fileinclude = require('gulp-file-include');
 const htmlmin = require('gulp-htmlmin');
+const size = require('gulp-size');
 
 
 //  обработка HTML
 const html = () => {
-  return src("./src/html/*.html")
+  return src('./src/html/*.html')
     .pipe(fileinclude())
+    .pipe(size({title: 'До сжатия'}))
     .pipe(htmlmin( { collapseWhitespace: true } ))
-    .pipe(dest("./public"));
+    .pipe(size({title: 'После сжатия'}))
+    .pipe(dest('./public'));
 }
 
 // задачи
